46f3d05117c27954ca970e0c7bb529dce79e6a08
jitujiten <sahuohjitu@gmail.com>
2023-01-19 16:00:13 +0530

    Added the react templete for practice

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import React from "react";
+
+
+const App=()=>{
+
+  let dummy_expenses = [
+    {
+      id: "e1",
+      title: "Toilet Paper",
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+      location: "mumbai",
+    },
+    {
+      id: "e2",
+      title: "New TV",
+      amount: 799.49,
+      date: new Date(2021, 2, 12),
+      location: "kolkata",
+    },
+    {
+      id: "e3",
+      title: "car Insurance",
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+      location: "banglore",
+    },
+    {
+      id: "e4",
+      title: "New Desk (Wooden)",
+      amount: 450,
+      date: new Date(2021, 5, 12),
+      location: "odisha",
+    },
+  ];
+
+
+
+
+ return <div>
+  hiiii
+ </div>
+}
+
+export default App;
---
+++ src/Component/Card/Card.js
@@ -0,0 +1,12 @@
+import "./Card.css";
+
+const Card=(props)=>{
+ const classses="card " + props.className;
+
+return (
+<div className={classses}>{props.children}</div>
+);
+
+}
+
+export default Card;
